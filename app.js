2d7146b2bbbc91f5fcfd0e1e00eb87045e88c543
Simon <simonnyu@gmail.com>
2016-12-02 19:10:53 +0800

    update app

--- app.js
+++ app.js
@@ -17,9 +17,24 @@ app.get('/db', function (request, response) {
             } else {
                 response.send(result);
             }
+            client.end(function (err){
+                if (err) console.error(err);
+            });
         });
-        client.end(function (err){
-            if (err) console.error(err);
+    });
+});
+app.get('/db_insert', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+        client.query('INSERT INTO test (\'text\') VALUE(\'insert test\')', function (err, result) {
+            if (err) {
+                console.error(err);
+                response.send("Error " + err);
+            } else {
+                response.send(result);
+            }
+            client.end(function (err){
+                if (err) console.error(err);
+            });
         });
     });
 });
