21298ff443623156c1750547ccec8c8a8ceb1360
Simon <simonnyu@gmail.com>
2016-12-02 18:17:53 +0800

    update app

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var express = require('express');
+var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get('/', function (request, response) {
+    response.send('Hello World!');
+});
+
+app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+        client.query('SELECT * FROM test_table', function (err, result) {
+            done();
+            if (err) {
+                console.error(err);
+                response.send("Error " + err);
+            } else {
+                response.send("successful");
+            }
+        });
+    });
 });
 
 app.listen(app.get('port'), function () {
