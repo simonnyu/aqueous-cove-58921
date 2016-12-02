528c79ceda4b8573c92f1921e33c47dc1d6ef732
Simon <simonnyu@gmail.com>
2016-12-02 18:25:57 +0800

    update app

--- app.js
+++ app.js
@@ -10,15 +10,11 @@ app.get('/', function (request, response) {
 
 app.get('/db', function (request, response) {
     pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-        client.query('SELECT * FROM test', function (err, result) {
-            done();
-            if (err) {
-                console.error(err);
-                response.send("Error " + err);
-            } else {
-                response.send("successful");
-            }
-        });
+        client
+            .query("SELECT * FROM test;")
+            .on('row',function(row){
+                response.send(JSON.stringify(row));
+            });
     });
 });
 
